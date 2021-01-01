cd5a6d06ba79cb0183e9f9458bffebcdef186b3d
Caitlin-Emily <caitlinemilyhuber@gmail.com>
2021-01-01 16:27:04 -0600

    pseudo code for function to generate password and function to write password to page for user

--- script.js
+++ script.js
@@ -6,9 +6,19 @@ var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var specialChar = ["!", "?", "@", "#", "^", "%", "&", ",", "*", "+", "-", ".", "/", "\", <", ">", "~"];
 
 // Declare variables for future use
-// use blank string for length variable, all others blank until later
+// use blank string for length variable, all others completely blank until later
 var length = "";
 var yespecial;
 var yesNumber;
 var yesLower;
 var yesUpper;
+
+// create function for generatePassword
+// within the function, you need the following:
+// prompt for password length and validation for prompt (needs to be between 8-128 characters)
+// confirmation of use of special characters, lower case, upper case, and/or numbers with validation for at least one
+// create string of characters for loop to choose from based on user input
+// randomly generate string from available characters (can use for loop)
+
+// write random string to password variable
+// write password variable to box in HTML
