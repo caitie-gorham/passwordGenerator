96bb812de3157d2b87ba3cfd78429b831920b04d
Caitlin-Emily <caitlinemilyhuber@gmail.com>
2021-01-03 19:11:48 -0600

    addition of while loop to condition parameter input and testing for generate button; fixed error for writing to #password by changing from crypto.getRandomValues() to math.random()

--- script.js
+++ script.js
@@ -16,23 +16,27 @@ var yesNumber;
 
 // create function for generatePassword
 function generatePassword() {
-    // set up variables first
-    var passConcat = [];
-    var passRandom = "";
-    
-    var yesLower = confirm("Click OK to confirm lower case letters");
-    var yesUpper = confirm("Click OK to confirm upper case letters");
-    var yesSpecial = confirm("Click OK to include special characters");
-    var yesNumber = confirm("Click OK to include numbers");
-    // need to add while loop here to make sure at least one parameter is chosen
     
     var length = (prompt("Choose a password length between 8-128"));
     while(length < 8 || length > 128) {
         alert("Password length must be between 8-128 characters");
         var length = (prompt("Choose a password length between 8-128"));
         } 
-   
-    // create string with all parameters that are confirmed - do these all need to be separate if statements?
+
+    var yesLower = confirm("Click OK to confirm lower case letters");
+    var yesUpper = confirm("Click OK to confirm upper case letters");
+    var yesSpecial = confirm("Click OK to include special characters");
+    var yesNumber = confirm("Click OK to include numbers");
+    
+    while(yesLower === false && yesUpper == false && yesSpecial == false && yesNumber == false) {
+        alert("You must click OK for at least one of the input parameters");
+        var yesLower = confirm("Click OK to confirm lower case letters");
+        var yesUpper = confirm("Click OK to confirm upper case letters");
+        var yesSpecial = confirm("Click OK to include special characters");
+        var yesNumber = confirm("Click OK to include numbers");
+    }
+
+    var passConcat = [];
     if (yesLower) {
         passConcat = passConcat.concat(lower);
     }
@@ -49,21 +53,20 @@ function generatePassword() {
         passConcat = passConcat.concat(number);
     }
 
-    // for loop to actually generate the passRandom variable 
+    var passRandom = "";
+   
     for (var i=0; i<length; i++) {
-        passRandom = passRandom + passConcat[Math.floor(window.crypto.getRandomValues()*passConcat.length)];
+        passRandom = passRandom + passConcat[Math.floor(Math.random()*passConcat.length)];
+        console.log(passRandom);
     }
-    
-    // return passRandom outside of for loop to only generate the last one 
     return passRandom;
-
 }
 
 // create function to create writePassword variable and write it to box on page (#password)
 function writePassword () {
-    var pass = generatePassword();
+    var password = generatePassword();
     var passwordString = document.querySelector("#password");
-    passwordString.value = pass;
+    passwordString.value = password;
 }
 
 generateBtn.addEventListener("click", writePassword);
